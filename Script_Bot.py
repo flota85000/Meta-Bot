3d21e85897f5046ec66a4d09eaa5cc3fdf8c59f5
Aub04 <164897249+Aub04@users.noreply.github.com>
2025-07-31 09:30:42 +0200

    Update Script_Bot.py

--- Script_Bot.py
+++ Script_Bot.py
@@ -61,7 +61,7 @@ def lancer_bot():
 
     print("✅ Fichier planning mis à jour après envois.")
     print("Heure actuelle : ", maintenant.strftime("%Y-%m-%d %H:%M:%S"))
-
+    print("TELEGRAM_TOKEN = ", repr(config.TELEGRAM_TOKEN))
 
 if __name__ == "__main__":
     lancer_bot()
