791fa79b8358e3cfd6e924b0b43d10407f26d65a
Aub04 <164897249+Aub04@users.noreply.github.com>
2025-07-31 07:58:55 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# === CONFIGURATION CENTRALE ===
+CHEMIN_CLE_JSON = "google_service_account.json"
+
+# Google Sheet – Fichiers
+FICHIER_CLIENTS = "Suivi Programme et heure client"
+FICHIER_PLANNING = "planning"
+FICHIER_PROGRAMMES = "Méta-université_Programmes"
+
+# Google Sheet – Feuilles internes
+FEUILLE_CLIENTS = "Clients"
+FEUILLE_PLANNING = "Planning"
+
+# === 🔐 API Telegram ===
+TELEGRAM_TOKEN = '7476882341:AAH2bDRNmmbHnySOHebB0JExMDD94z3IkDo'
+
+# === ⏱️ Autres paramètres
+NB_JOURS_GENERATION = 30  # Nombre de jours de planning à générer
+FUSEAU_HORAIRE = "Europe/Paris"
+LANGUE = "fr_FR.UTF-8"
+
+# === 📓 Logger / erreurs
+ACTIVER_LOG = True
+FICHIER_LOG = "journal_erreurs.log"
